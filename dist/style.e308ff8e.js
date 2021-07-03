1a43205607f777793a2fd77980b947d29ffe4462
zq <zhaoqin_yin@163.com>
2021-07-03 12:38:11 +0800

    update build

--- dist/style.e308ff8e.js
+++ dist/style.e308ff8e.js
@@ -117,41 +117,11 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"main.js":[function(require,module,exports) {
-var html = document.querySelector('#html');
-var style = document.querySelector('#style');
-var string = "\n/*\u4F60\u597D\uFF0C\u6211\u53EB\u5C0F\u94A6\n*\u63A5\u4E0B\u6765\u6211\u6F14\u793A\u4E00\u4E0B\u6211\u7684\u524D\u7AEF\u529F\u5E95\n*\u9996\u5148\u6211\u8981\u51C6\u5907\u4E00\u4E2Adiv\n*/\n#div1{\n    border: 1px solid red;\n    width:200px;\n    height:200px;\n}\n/*\u63A5\u4E0B\u6765\u6211\u628Adiv\u53D8\u6210\u4E00\u4E2A\u516B\u5366\u56FE\n*\u6CE8\u610F\u770B\u597D\u4E86\n*\u9996\u5148\uFF0C\u628Adiv\u53D8\u6210\u4E00\u4E2A\u5706\n*/\n#div1{\n    border-radius:50%;\n    box-shadow:0 0 3px rgba(0,0,0,0.5);\n    border:none;\n}\n/*\u516B\u5366\u662F\u9634\u9633\u5F62\u6210\n*\u4E00\u9ED1\u4E00\u767E\n*/\n#div1{\n    background:linear-gradient(90deg,rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);\n}\n/* \u52A0\u4E24\u4E2A\u7403 */\n#div1::before{\n    width:100px;\n    height:100px;\n    top:0;\n    left:50%;\n    transform:translateX(-50%);\n    background:#000;\n    border-radius:50%;\n    background:radial-gradient(circle,rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%, rgba(0,0,0,1) 100%);\n}\n\n#div1::after{\n    width:100px;\n    height:100px;\n    bottom:0;\n    left:50%;\n    transform:translateX(-50%);\n    background:#fff;\n    border-radius:50%;\n    background:radial-gradient(circle,rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%, rgba(0,0,0,1) 100%);\n}\n"; // string.replace('\n','<br>')只会把第一个回车变<br>
-// 因此我们需要用正则表达式，但是这个方法也有错，因此要重写
-// string = string.replace(/\n/g,"<br>")
-// 新的解决办法是在下面的代码里判断回车
+})({"style.css":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
 
-var string2 = '';
-var n = 0;
-
-var step = function step() {
-  // 用setTimeout模拟setInterval，这样做的好处就是可以随时停止
-  setTimeout(function () {
-    if (string[n] === '\n') {
-      string2 += '<br>';
-    } else if (string[n] === ' ') {
-      string2 += '&nbsp;';
-    } else {
-      string2 = string2 + string[n];
-    }
-
-    html.innerHTML = string2;
-    style.innerHTML = string.substring(0, n);
-    window.scrollTo(0, 99999);
-    html.scrollTo(0, 99999);
-
-    if (n < string.length - 1) {
-      n = n + 1;
-      step();
-    }
-  }, 0);
-};
-
-step();
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
 },{}],"../../../../.config/yarn/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 
 },{}]
